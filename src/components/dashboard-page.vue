dfaf5651a7a0afd1470b336e0ee344e115d3800f
User <paadams@nmu.edu>
2017-09-30 17:19:37 -0400

    added expansion panels to dashboard-page

--- src/components/dashboard-page.vue
+++ src/components/dashboard-page.vue
@@ -1,25 +1,43 @@
 <template>
-  <v-layout>
-      <v-flex xs12 sm6 offset-sm3>
-          <h2>Dashboard</h2>
-          <v-card light>
-              <v-card-title primary-title>
-                  <div>
-                      <h3 class="headline mb-0">Create a whiteboard</h3>
-                  </div>
-              </v-card-title>
-          </v-card>
-      </v-flex>
-       <v-flex xs12 sm6 offset-sm3>
-          <v-card light>
-              <v-card-title primary-title>
-                  <div>
-                      <h3 class="headline mb-0">Join a whiteboard</h3>
-                  </div>
-              </v-card-title>
-          </v-card>
-      </v-flex>
-  </v-layout>
+    <v-container>
+        <h3>Dashboard</h3>
+        <v-layout class="dashboard-layout">
+            <v-flex
+                class="flex"
+                xs5
+            >      
+                <v-expansion-panel popout>
+                    <v-expansion-panel-content>
+                        <div slot="header">Create a whiteboard</div>
+                        <v-card>
+                            <v-card-text>
+                                <div>
+                                    <h3>Create whiteboard form</h3>
+                                </div>
+                            </v-card-text>
+                        </v-card>
+                    </v-expansion-panel-content>
+                </v-expansion-panel>
+            </v-flex>
+            <v-flex
+                class="flex"
+                xs5
+            >
+               <v-expansion-panel popout>
+                    <v-expansion-panel-content>
+                        <div slot="header">Join a whiteboard</div>
+                        <v-card>
+                            <v-card-text>
+                                <div>
+                                    <h3>list of whiteboars</h3>
+                                </div>
+                            </v-card-text>
+                        </v-card>
+                    </v-expansion-panel-content>
+                </v-expansion-panel>
+            </v-flex>
+        </v-layout>
+    </v-container>
 </template>
 <script>
 export default {
@@ -27,6 +45,15 @@ export default {
 }
 </script>
 <style scoped>
-
+    .dashboard-layout {
+        margin-top: 60px;
+    }
+    h3 {
+        text-align: center;
+    }
+    .flex {
+        margin-left: 5%;
+        padding: 10px;
+    }
 </style>
 
