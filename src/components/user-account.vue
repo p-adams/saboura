bf8100c01fe30edf04290d2b57828af05a134fa5
Paul Adams <paadams@nmu.edu>
2017-10-11 21:22:27 -0400

    add user-layout and slots

--- src/components/user-account.vue
+++ src/components/user-account.vue
@@ -1,11 +1,18 @@
 <template>
-  <div>
-      User account
-  </div>
+  <v-container :style="{border: '1px solid red'}">
+    <user-layout>
+        <h5 slot="header">User Account</h5>
+        <p>some content</p>
+    </user-layout>
+  </v-container>
 </template>
 <script>
+import UserLayout from './user-layout'
 export default {
-  name: 'UserAccount'
+  name: 'UserAccount',
+  components: {
+      UserLayout
+  }
 }
 </script>
 
---
+++ src/components/user-layout.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-container :style="{border: '1px solid green'}">
+      <header>
+          <slot name="header"></slot>
+      </header>
+      <main>
+          <slot></slot>
+      </main>
+  </v-container>
+</template>
+<script>
+export default {
+  name: 'UserLayout'
+}
+</script>
+<style scoped>
+    
+</style>
+
+
