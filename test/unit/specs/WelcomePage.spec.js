9a08371480cc5a2ea7770c74917100fdb807ec83
Paul Adams <paadams@nmu.edu>
2017-11-29 12:13:19 -0500

    add Vuetify to test

--- test/unit/specs/WelcomePage.spec.js
+++ test/unit/specs/WelcomePage.spec.js
@@ -5,7 +5,7 @@ describe("WelcomePage.vue", () => {
   it("should render corrent app tagline", () => {
     const Constructor = Vue.extend(WelcomePage);
     const vm = new Constructor().$mount();
-    expect(vm.$el.querySelector(".tagline h6").textContent).to.equal(
+    expect(vm.$el.querySelector(".tagline").textContent).to.equal(
       "Welcome to Saboura"
     );
   });
