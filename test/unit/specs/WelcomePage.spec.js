3ff7c34669e4b568a1afff14ba02e24e104becbb
Paul Adams <paadams@nmu.edu>
2017-11-29 12:43:07 -0500

    write passing unit test

--- test/unit/specs/WelcomePage.spec.js
+++ test/unit/specs/WelcomePage.spec.js
@@ -2,11 +2,11 @@ import Vue from "vue";
 import WelcomePage from "@/components/welcome-page";
 
 describe("WelcomePage.vue", () => {
-  it("should render corrent app tagline", () => {
+  it("should render correct app tagline", () => {
     const Constructor = Vue.extend(WelcomePage);
     const vm = new Constructor().$mount();
     expect(vm.$el.querySelector(".tagline").textContent).to.equal(
-      "Welcome to Saboura"
+      "Our collaborative, interactive whiteboards make sharing ideas with colleagues a breeze"
     );
   });
 });
